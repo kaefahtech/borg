b390c37b484cbd87f4376948d0bd1a1fd2f84789
Kojo Amponsah Efah <kojampef4@gmail.com>
2020-07-18 02:01:38 -0400

    Deleted tests/test_docker_manager.py

--- tests/test_docker_manager.py
+++
@@ -1,6 +0,0 @@
-class TestDockerManager:
-	def test_can_instantiate_docker_manager_class(self):
-		""" Test DockerManager class """
-		from app.docker_manager import DockerManager
-		docker_manager = DockerManager()
-		assert isinstance(docker_manager, DockerManager)
