a13a493d1a76adc32efe4336c47ec5667fb30910
Kojo Amponsah Efah <kojampef4@gmail.com>
2020-07-18 03:00:35 -0400

    Moved Installer test to dedicated test file and added TODO items

--- tests/test_docker_borg.py
+++ tests/test_docker_borg.py
@@ -1,5 +1,4 @@
 from app.docker_borg import DockerManager
-from app.installer import Installer
 
 
 class TestDockerManager:
@@ -9,17 +8,30 @@ class TestDockerManager:
 		assert isinstance(docker_manager, DockerManager)
 
 
-class TestInstaller:
-	def test_can_instantiate_installer_base_class(self):
-		""" Test Installer base class """
-		Installer.__abstractmethods__ = set()
-		installer = Installer()
-		assert isinstance(installer, Installer)
-		installer.install
+class TestDockerInstaller:
+	def test_can_instantiate_docker_installer_class(self):
+		""" Test DockerInstaller class """
+		from app.docker_borg import DockerInstaller
+		docker_installer = DockerInstaller()
+		assert isinstance(docker_installer, DockerInstaller)
 
-# class TestDockerInstaller:
-# 	def test_can_instantiate_docker_installer_class(self):
-# 		""" Test DockerInstaller class """
-# 		from app.docker_borg import DockerInstaller
-# 		docker_installer = DockerInstaller()
-# 		assert isinstance(docker_installer, DockerInstaller)
+# TODO: Test that we can remove old docker versions from the system
+# TODO: Test that we can update the system
+# TODO: Test that we can install required packages to allow repo use over HTTPS on system
+# TODO: Test that we can add the required GPG key to the system
+# TODO: Test that we can verify the fingerprint of the installation on the system
+# TODO: Test that we can set up a stable repository with nightly and test repositories
+# TODO: Test that we can install a specified version or the latest docker engine on the system
+# TODO: Test that we can upgrade a Docker Engine on a system
+# TODO: Test that we can set Docker up to be managed by non-root user on a system
+# TODO: Test that we can configure Docker to start on boot on a system
+# TODO: Test that we can use a different storage engine
+# TODO: Test that we can configure a default logging driver on a system
+# TODO: Test that we can configure where the Docker daemon listens for connections on a system
+# TODO: Test that we can troubleshoot kernel compatibility issues
+# TODO: Test that we can resolve IP forwarding problems
+# TODO: Test that we can specify DNS servers for Docker
+# TODO: Test that we can allow access to the remote API through a firewall
+#
+
+
