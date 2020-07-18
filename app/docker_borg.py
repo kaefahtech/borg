530c5e12e0fe5ae7949034c9a7b8dbce117f1209
Kojo Amponsah Efah <kojampef4@gmail.com>
2020-07-18 02:58:23 -0400

    Added install method to implement Installer class install abstract method

--- app/docker_borg.py
+++ app/docker_borg.py
@@ -1,8 +1,18 @@
 """ Manage Docker installation on systems """
+from .installer import Installer
 
 
 class DockerManager:
 	""" Manages Docker installations on servers """
+
+
+class DockerInstaller(Installer):
+	""" DockerInstaller class to implement Installer interface """
+
+	def install(self):
+		""" install method to implement abstract install method
+		    from Installer interface class
+		"""
 
 
 
